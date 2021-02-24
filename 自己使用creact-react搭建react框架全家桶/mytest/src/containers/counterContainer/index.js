eb8671f8c193c5ea7e704f25a8fc7744aab7b5cd
lisicheng <lisicheng@ewell.cc>
2021-02-24 17:41:55 +0800

    Revert "自己用3套件组装"
    This reverts commit 0fd94e637300b9048cd0a3f8011dd06094477b56.

--- 自己使用creact-react搭建react框架全家桶/mytest/src/containers/counterContainer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Counter from '../../components/counter'
-export default function index() {
-    return (
-        <>
-           <Counter number={1}/> 
-        </>
-    )
-}
--- 自己使用creact-react搭建react框架全家桶/mytest/src/containers/studentContainer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Student from '../../components/student'
-export default function index() {
-    return (
-        <>
-           <Student name='里斯' sex = '男'/> 
-        </>
-    )
-}
